bd178d1dfebc909c0c9e1da15ebcdeae80e6f61b
lju <veryveryeasily@gmail.com>
2021-12-02 23:16:22 -0800

    get result for first problem!

--- src/AdventOfCode.jl
+++ src/AdventOfCode.jl
@@ -1,5 +1,21 @@
 module AdventOfCode
 
-greet() = print("Hello World!")
+using Pipe
+
+function readinput()
+    strs = open("input.txt", "r") do f
+        readlines(f)
+    end
+    lines = map(x -> parse(Int, x), filter(x -> x != "", strs))
+    return lines
+end
+
+function main()
+    lines = readinput()
+    result = @pipe zip(lines[1:end-1], lines[2:end]) |>
+        map(x -> x[1] < x[2], _) |>
+        sum(_)
+    return result
+end
 
 end # module
