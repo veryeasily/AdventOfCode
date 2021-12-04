8fd5801ca0267b8fcb674ac25ba062afec819ca5
lju <veryveryeasily@gmail.com>
2021-12-03 22:53:45 -0800

    add in code for day_01_2

--- src/AdventOfCode.jl
+++ src/AdventOfCode.jl
@@ -10,10 +10,23 @@ function readinput()
     return lines
 end
 
-function main()
+function day_01_1()
     lines = readinput()
     return @pipe (
         zip(lines[1:end-1], lines[2:end]) |>
+            map(x -> x[1] < x[2], _) |>
+            sum(_)
+    )
+end
+
+function day_01_2()
+    lines = readinput()
+    sums = @pipe (
+        zip(lines[1:end-2], lines[2:end-1], lines[3:end]) |>
+            map(x -> sum(x), _)
+    )
+    return @pipe (
+        zip(sums[1:end-1], sums[2:end]) |>
             map(x -> x[1] < x[2], _) |>
             sum(_)
     )
